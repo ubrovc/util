f43af38135cb7275bf796f40cdd900f48ad87f5d
u.brovc <u.brovc@iskratel.si>
2022-08-22 16:08:39 +0200

    gin temp solution

--- fsm/fsm.go
+++ fsm/fsm.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"strings"
 
-	"github.com/free5gc/util/fsm/logger"
+	"github.com/ubrovc/util/fsm/logger"
 )
 
 type (
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/free5gc/util
+module github.com/ubrovc/util
 
 go 1.14
 
